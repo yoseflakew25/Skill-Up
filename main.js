f6bd76de91eb1ff3777c024e50204f3e5a9f4077
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-10-22 17:11:33 +0300

    .

---
+++ main.js
@@ -0,0 +1,26 @@
+// Add scroll event listener
+window.addEventListener("scroll", function () {
+  // Get the header element
+  const header = document.getElementById("header");
+
+  // Calculate the scroll position
+  const scrollPosition = window.scrollY || window.pageYOffset;
+
+  // Define the threshold for the glass effect
+  const threshold = 100;
+
+  // Add or remove the glass class based on the scroll position
+  if (scrollPosition >= threshold) {
+    header.classList.add(
+      "bg-opacity-75",
+      "backdrop-filter",
+      "backdrop-blur-lg"
+    );
+  } else {
+    header.classList.remove(
+      "bg-opacity-75",
+      "backdrop-filter",
+      "backdrop-blur-lg"
+    );
+  }
+});
